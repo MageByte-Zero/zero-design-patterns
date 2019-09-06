f0cccd47be82cd521c4ff076d871e6ef008c2d62
JavaStorm <zero_develop@163.com>
2019-09-06 21:37:48 +0800

    责任链模式

--- src/main/java/com/zero/design/actions/dutychain/GeneralManagerHandler.java
+++ src/main/java/com/zero/design/actions/dutychain/GeneralManagerHandler.java
@@ -18,7 +18,7 @@ public class GeneralManagerHandler extends AbstractLeaderHandler {
 
         if(days <= 30) { //如果满足10天内的要求，经理直接审批
             System.out.println("员工" + name + "请假" + days + "天，理由：" + reason);
-            System.out.println("经理" + this.handlerName + "审批通过");
+            System.out.println("总办" + this.handlerName + "审批通过");
         } else {
             System.out.println("请假天数过多，总办" + this.handlerName + "没法处理");
             if(this.nextLeaderHandler != null) { //否则，如果链上存在下一个Leader，就让他处理
