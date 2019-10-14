b8ba939ca50980d8ec0dc066b48bc6a41b3e347c
JavaStorm <zero_develop@163.com>
2019-10-14 22:04:30 +0800

    设计模式综合-装饰器模式不改动鸭子叫声代码下统计叫的次数

--- src/main/java/com/zero/headfirst/verb/DuckSimulation.java
+++ src/main/java/com/zero/headfirst/verb/DuckSimulation.java
@@ -1,6 +1,7 @@
 package com.zero.headfirst.verb;
 
 import com.zero.headfirst.verb.adapter.GooseAdapter;
+import com.zero.headfirst.verb.decorator.QuackCounter;
 import com.zero.headfirst.verb.duck.*;
 import com.zero.headfirst.verb.goose.Goose;
 
@@ -17,16 +18,20 @@ public class DuckSimulation {
      * 模拟屏幕展示功能
      */
     private void simulate() {
-        Quackable redheadDuck = new RedheadDuck();
-        Quackable mallarDuck = new MallarDuck();
-        Quackable rubberDuck = new RubberDuck();
-        Quackable duckCall = new DuckCall();
+        Quackable redheadDuck = new QuackCounter(new RedheadDuck());
+        Quackable mallarDuck = new QuackCounter(new MallarDuck());
+        Quackable rubberDuck = new QuackCounter(new RubberDuck());
+        Quackable duckCall = new QuackCounter(new DuckCall());
+        //不想把天鹅的叫声统计，所以不用装饰器装饰
         GooseAdapter gooseAdapter = new GooseAdapter(new Goose());
+        System.out.println("使用装饰器模式后，统计叫的次数，不包含天鹅");
+
         simulate(redheadDuck);
         simulate(mallarDuck);
         simulate(rubberDuck);
         simulate(duckCall);
         simulate(gooseAdapter);
+        System.out.println("一共叫了 " + QuackCounter.getQuacks() + " 次");
     }
 
     private void simulate(Quackable quackable) {
---
+++ src/main/java/com/zero/headfirst/verb/decorator/QuackCounter.java
@@ -0,0 +1,29 @@
+package com.zero.headfirst.verb.decorator;
+
+import com.zero.headfirst.verb.duck.Quackable;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 装饰器模式，发出叫声的时候同时记录次数.这样我们就不必修改每个鸭子发出声音的方法
+ */
+public class QuackCounter implements Quackable {
+
+    private Quackable quack;
+
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    public QuackCounter(Quackable quack) {
+        this.quack = quack;
+    }
+
+    @Override
+    public void quack() {
+        quack.quack();
+        atomicInteger.incrementAndGet();
+    }
+
+    public static AtomicInteger getQuacks() {
+        return atomicInteger;
+    }
+}
