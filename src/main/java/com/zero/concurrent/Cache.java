c3a370e9342c7694d6f1bb9f7959683413522b96
lijianqing <lijianqing@linklogis.com>
2019-10-21 20:30:28 +0800

    feat(ReadWriteLock): 写锁降级

--- src/main/java/com/zero/concurrent/Cache.java
+++ src/main/java/com/zero/concurrent/Cache.java
@@ -24,49 +24,61 @@ public class Cache<String, V> {
 
     /**
      * 读取缓存,若缓存不存在则查找数据库并放到缓存
+     *
      * @param key
      * @return
      */
     public V get(String key) {
-        V value = null;
-        //读取缓存
         readLock.lock();
+        System.out.println("开始读取缓存数据....." + Thread.currentThread().getName());
+        V value = DATA_MAP.get(key);
+        //缓存中不存在，释放读锁获取写锁
+        if (Objects.isNull(value)) {
+            // 若 value 为空则先释放读锁，并且让该线程获取写锁，而其他线程只能等待该写锁释放
+            // 必须先释放读锁才能去获取写锁，读锁还没有释放，此时获取写锁，会导致写锁永久等待，最终导致相关线程都被阻塞，永远也没有机会被唤醒。锁的升级是不允许的，这个你一定要注意。
+            readLock.unlock();
+            writeLock.lock();
+            // 再次检查状态，因为其他线程可能已经获取到写锁且更新了状态
+            try {
+                if (Objects.isNull(value)) {
+                    //模拟查询数据库
+                    value = (V) "queryFromDB";
+                    DATA_MAP.put(key, value);
+                }
+                // 通过在释放写锁之前获取读锁来实现锁降级，降级为读锁
+                readLock.lock();
+            } finally {
+                // 释放写锁，依然持有读锁
+                writeLock.unlock();
+            }
+        }
+
+        //当前还持有读锁,所以最后还要释放读锁
         try {
-            value = DATA_MAP.get(key);
+            System.out.println(Thread.currentThread().getName() + "-- 读 : {key:" + key + ",value: " + value + "}");
+            return value;
         } finally {
+            System.out.println("结束读取缓存数据....." + Thread.currentThread().getName());
             readLock.unlock();
         }
-        // 缓存中存在，直接返回
-        if (Objects.nonNull(value)) {
-            return value;
-        }
-        writeLock.lock();
-        try {
-            // 再次验证是否为空，可能其他写线程已经查询郭数据库写到缓存中了，后续再次获取到写锁的线程就不用再次查数据库
-            value = DATA_MAP.get(key);
-            if (Objects.isNull(value)) {
-                //模拟查询数据库
-                value = (V) "queryFromDB";
-                DATA_MAP.put(key, value);
-            }
-        } finally {
-            writeLock.unlock();
-        }
-        return value;
 
     }
 
     /**
      * 写缓存
+     *
      * @param key
      * @param value
      * @return
      */
     public V put(String key, V value) {
         writeLock.lock();
+        System.out.println("开始写入缓存数据....." + Thread.currentThread().getName());
         try {
+            System.out.println(Thread.currentThread().getName() + "-- 写 :  {key:" + key + ",value: " + value + "}");
             return DATA_MAP.put(key, value);
         } finally {
+            System.out.println("结束写入缓存数据....." + Thread.currentThread().getName());
             writeLock.unlock();
         }
     }
