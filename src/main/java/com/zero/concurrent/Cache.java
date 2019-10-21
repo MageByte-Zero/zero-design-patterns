7d608cb95a4f5989f8ff34bcc821c5535e8da013
lijianqing <lijianqing@linklogis.com>
2019-10-21 17:03:20 +0800

    feat(ReadWriteLock): 读取缓存若不存在则从数据库加载放入缓存

--- src/main/java/com/zero/concurrent/Cache.java
+++ src/main/java/com/zero/concurrent/Cache.java
@@ -2,12 +2,13 @@ package com.zero.concurrent;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-public class Cache<K, V> {
+public class Cache<String, V> {
 
-    private final Map<K, V> DATA_MAP = new HashMap<>();
+    private final Map<String, V> DATA_MAP = new HashMap<>();
 
     private final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 
@@ -22,17 +23,37 @@ public class Cache<K, V> {
     private final Lock writeLock = readWriteLock.writeLock();
 
     /**
-     * 读取缓存
+     * 读取缓存,若缓存不存在则查找数据库并放到缓存
      * @param key
      * @return
      */
-    public V get(K key) {
+    public V get(String key) {
+        V value = null;
+        //读取缓存
         readLock.lock();
         try {
-            return DATA_MAP.get(key);
+            value = DATA_MAP.get(key);
         } finally {
             readLock.unlock();
         }
+        // 缓存中存在，直接返回
+        if (Objects.nonNull(value)) {
+            return value;
+        }
+        writeLock.lock();
+        try {
+            // 再次验证是否为空，可能其他写线程已经查询郭数据库写到缓存中了，后续再次获取到写锁的线程就不用再次查数据库
+            value = DATA_MAP.get(key);
+            if (Objects.isNull(value)) {
+                //模拟查询数据库
+                value = (V) "queryFromDB";
+                DATA_MAP.put(key, value);
+            }
+        } finally {
+            writeLock.unlock();
+        }
+        return value;
+
     }
 
     /**
@@ -41,7 +62,7 @@ public class Cache<K, V> {
      * @param value
      * @return
      */
-    public V put(K key, V value) {
+    public V put(String key, V value) {
         writeLock.lock();
         try {
             return DATA_MAP.put(key, value);
