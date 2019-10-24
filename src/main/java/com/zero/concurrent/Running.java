1d1a8e61d96cfee241ee5dcf684f3a66fe49ac98
lijianqing <lijianqing@linklogis.com>
2019-10-24 17:04:20 +0800

    feat (并发): CountDownLatch 注释

--- src/main/java/com/zero/concurrent/Running.java
+++ src/main/java/com/zero/concurrent/Running.java
@@ -9,15 +9,17 @@ public class Running {
     public static void main(String[] args) throws InterruptedException {
 
         int n = 6;
+        // 初始化计数器为 6
         CountDownLatch doneSignal = new CountDownLatch(n);
         Executor e = Executors.newFixedThreadPool(6);
 
+        //模拟 6 个线程
         for (int i = 0; i < n; ++i) {
             e.execute(new Runner("doneSignal" + i, doneSignal));
         }
         //等待所有跑男就位
         System.out.println("所有运动员就位，计时开始");
-        // 当 doneSignal 每次执行 countDown 操作，变成了 0 之后所有线程唤醒执行
+        // 当 doneSignal 每次执行 countDown - 1 操作，变成了 0 之后所有线程唤醒执行
         doneSignal.await();
     }
 
@@ -38,7 +40,7 @@ class Runner implements Runnable {
         try {
             System.out.println("name = " + name + "开跑");
         } finally {
-            //
+            // 对计数器 - 1
             doneSignal.countDown();
         }
     }
