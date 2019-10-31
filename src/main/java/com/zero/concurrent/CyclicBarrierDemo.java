7d5869474e4ddb54c3028d1e301ca6d4f48dee6e
lijianqing <lijianqing@linklogis.com>
2019-10-31 18:04:25 +0800

    docs(CyclicBarrier): 添加注释

--- src/main/java/com/zero/concurrent/CyclicBarrierDemo.java
+++ src/main/java/com/zero/concurrent/CyclicBarrierDemo.java
@@ -20,6 +20,7 @@ public class CyclicBarrierDemo {
             executorService.execute(() -> {
                 try {
                     System.out.println(Thread.currentThread().getName() + "运动员，准备就绪");
+                    // 每个运动员执行到这就会 对 N - 1，变为 0 则放一波线程运行，然后重置 N
                     cyclicBarrier.await();
                     System.out.println(Thread.currentThread().getName() + "运动员，开跑");
                     TimeUnit.SECONDS.sleep(current);
